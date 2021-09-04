79b07c26913f6937d53c127a68e4afe359bf061f
maribardella <mariane.bardella@gmail.com>
2021-09-04 10:31:15 -0300

    Criação dos primeiros arquivos para o back

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+require('./controllers/authController')(app);
+
+app.listen(3000);
---
+++ src/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+//Definição dos campos do banco de dados
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+});
+
+const User = mongoose.model('User',UserSchema);
+
+module.exports = User;
