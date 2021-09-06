fd37bbf729f1c5a7832810bc3eedb7f670e335ee
alissoncorsair <85128038+alissoncorsair@users.noreply.github.com>
2021-09-06 16:06:55 -0300

    mesa circular - seleção unica

--- testes/circulo para possivel mesa/script.js
+++ testes/circulo para possivel mesa/script.js
@@ -3,8 +3,13 @@ const bigCircle = document.querySelector(".main");
 
 
 
-bigCircle.addEventListener("click", (e) =>{
-    if(e.target.classList.contains("circle") && !e.target.classList.contains("occupied")) {
+bigCircle.addEventListener("click", (e) => {
+
+    if (e.target.classList.contains("circle") && !e.target.classList.contains("occupied")) {
+        const selectedCircles = document.querySelector(".selected");
+        if (selectedCircles) {
+            selectedCircles.classList.remove("selected");
+        }
         e.target.classList.toggle("selected");
     }
 });
