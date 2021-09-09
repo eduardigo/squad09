68298b6d32b2a2ed1ca2b4167b2cbec07801591f
Eduardo Felipe Ardigo Braga <79773073+eduardigo@users.noreply.github.com>
2021-09-09 14:47:43 -0300

    Delete back/backAgendamento/database directory

--- back/backAgendamento/database/index.js
+++
@@ -1,7 +0,0 @@
-//const mongoose = require ('mongoose');
-
-//mongoose.connect('mongodb://localhost/noderest', {useNewUrlParser: true, useUnifiedTopology: true});
-
-//mongoose.Promise = global.Promise;
-
-//module.exports = mongoose;
