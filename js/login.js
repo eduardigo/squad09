06d8153efcf568e4351d6954e2a63d7d0b90f7af
alissoncorsair <85128038+alissoncorsair@users.noreply.github.com>
2021-09-11 04:18:10 -0300

    incompleto - login enviando pro back

--- js/login.js
+++ js/login.js
@@ -3,13 +3,10 @@ const email = document.getElementById('digitar-email');
 const password = document.getElementById('digitar-senha');
 
 
-
 // ação disparada ao apertar o botão de login 
 loginForm.addEventListener('submit', function (e) {
-
     //impede a página de apagar os valores do form/recarregar a página
     e.preventDefault();
-
 // teste para saber se o e-mail e a senha estão passando
     console.log(email.value);
     console.log(password.value);
@@ -22,12 +19,14 @@ loginForm.addEventListener('submit', function (e) {
         body: JSON.stringify(data)
     }).then(function (response) {
         return response.json();
-
-    }).then(function (data) {
-        // passando o token do usuário para a localStorage (para uma possível funcionalidade futura)
-     localStorage.setItem("token",data.token)
+    // }).then(function (data) {
+    //     // passando o token do usuário para a localStorage (para uma possível funcionalidade futura)
+    //  localStorage.setItem("token",data.token)
     }).catch(function (error) {
         console.log(error);
     })
-    // console.log(localStorage.token);
 });
+
+
+
+
