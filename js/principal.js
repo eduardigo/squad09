880d6344ffa89370ccff0ea062a058f468e5fa74
alissoncorsair <85128038+alissoncorsair@users.noreply.github.com>
2021-09-10 23:38:25 -0300

    começo do login(front) com o back

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,25 @@
 
+const email = document.getElementById('digitar-email');
+const password = document.getElementById('digitar-senha');
+
+
+myForm.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+
+    const data = { email: email.value, password: password.value };
+
+    fetch('http://localhost:3000/auth/autenticacao', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    }).then(function (response) {
+        return response.json();
+
+    }).then(function (data) {
+     localStorage.setItem("token",data.token)
+    }).catch(function (error) {
+        console.log(error);
+    })
+    // console.log(localStorage.token);
+});
