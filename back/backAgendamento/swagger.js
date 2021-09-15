0d6a6c58a311981990d1121ab450c5cb53c51964
Eduardo Felipe Ardigo Braga <79773073+eduardigo@users.noreply.github.com>
2021-09-14 21:03:43 -0300

    Delete swagger.js

--- back/backAgendamento/swagger.js
+++
@@ -1,8 +0,0 @@
-const swaggerAutogen = require('swagger-autogen')();
-
-const outputFile = './swagger_output.json';
-const endpointsFiles = ['./routes/agendamento.routes.js', './routes/posto.routes.js', './routes/unidade.routes.js', './controllers/authController.js', './controllers/projectController.js'];
-
-swaggerAutogen(outputFile, endpointsFiles).then(() => {
-    require('./index.js');
-});
